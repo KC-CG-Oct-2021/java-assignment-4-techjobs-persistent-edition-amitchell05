2173aa2284e3d2d8d46f94e4827200e87cc0f6d2
Angela Mitchell <anglarenmit@gmail.com>
2022-07-01 02:22:44 -0500

    persist employers and skills

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -1,12 +1,21 @@
 package org.launchcode.techjobs.persistent.models;
 
 import java.util.Objects;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
-
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank(message = "Name is required.")
+    @Size(min = 3, max = 120, message = "Name must be between 3 and 120 characters.")
     private String name;
 
     public int getId() {
